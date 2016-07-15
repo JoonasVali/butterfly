b0c9805ed6bc9c0c99192cf92a215ce423e87ae2
Joonas Vali <joonas.wali@gmail.com>
2016-07-15 12:17:10 +0300

    vision seems to work

--- src/main/java/ee/joonasvali/butterfly/simulation/actor/vision/VisibleObject.java
+++ src/main/java/ee/joonasvali/butterfly/simulation/actor/vision/VisibleObject.java
@@ -7,7 +7,7 @@ import ee.joonasvali.butterfly.simulation.actor.Actor;
  * @author Joonas Vali July 2016
  */
 public class VisibleObject {
-  private final double relativeRotationToActor;
+  private final double relativeRotationToObject;
   private final double distance;
   private final double diameter;
 
@@ -16,16 +16,27 @@ public class VisibleObject {
   }
 
   private static double calculateDistance(Actor observer, Physical other) {
-    return 0;
+    double omidX = other.getX() + other.getDiameter() / 2;
+    double omidY = other.getY() + other.getDiameter() / 2;
+    double x = observer.getX() + observer.getDiameter() / 2;
+    double y = observer.getY() + observer.getDiameter() / 2;
+    return Math.sqrt(Math.pow(omidY - y, 2) + Math.pow(omidX - x, 2)) - (observer.getDiameter() / 2 + other.getDiameter() / 2);
   }
 
   private static double calculateRotation(Actor observer, Physical other) {
-    return 0;
+    double rotationOffset = observer.getRotation();
+    double omidX = other.getX() + other.getDiameter() / 2;
+    double omidY = other.getY() + other.getDiameter() / 2;
+    double x = observer.getX() + observer.getDiameter() / 2;
+    double y = observer.getY() + observer.getDiameter() / 2;
+    double deltaY = omidY - y;
+    double deltaX = omidX - x;
+    return Math.toDegrees(Math.atan2(deltaY, deltaX)) - rotationOffset;
   }
 
-  public VisibleObject(double relativeRotationToActor, double distance, double diameter) {
+  public VisibleObject(double relativeRotationToObject, double distance, double diameter) {
     this.diameter = diameter;
-    this.relativeRotationToActor = relativeRotationToActor;
+    this.relativeRotationToObject = relativeRotationToObject;
     this.distance = distance;
   }
 
@@ -33,8 +44,8 @@ public class VisibleObject {
     return distance;
   }
 
-  public double getRelativeRotationToActor() {
-    return relativeRotationToActor;
+  public double getRelativeRotationToObject() {
+    return relativeRotationToObject;
   }
 
   public double getDiameter() {
