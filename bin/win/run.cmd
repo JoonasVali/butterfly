332e96eeac6afff833d0f7dd85f3106bd52bde97
Joonas Vali <joonas.wali@gmail.com>
2016-07-27 21:56:47 +0300

    fix launchers

--- bin/win/run.cmd
+++ bin/win/run.cmd
@@ -1,3 +1,3 @@
 @echo off
 SET ${name.uppercase}_HOME=..
-java -jar -Djava.library.path=./%${name.uppercase}_HOME%/sl-libs/ -Xmx2048M -Dbutterfly.properties.path=../butterfly-win.properties ./%${name.uppercase}_HOME%/lib/${artifactId}.jar
+java -jar -Djava.library.path=./%${name.uppercase}_HOME%/sl-libs/ -Xmx2048M -Dbutterfly.properties.path=./%${name.uppercase}_HOME%/butterfly-win.properties ./%${name.uppercase}_HOME%/lib/${artifactId}.jar
